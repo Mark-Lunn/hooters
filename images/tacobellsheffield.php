4b5f1d2c9565fc3509be1a3fcb7ff97fc0d126dd
Declan Harrop <dh@38co.io>
2020-01-06 21:27:00 +0000

    Base - 1.0.0
    Added Files

---
+++ images/tacobellsheffield.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<link rel=Ã¢â‚¬ÂiconÃ¢â‚¬Â href=Ã¢â‚¬Âfavicon.icoÃ¢â‚¬Â type=Ã¢â‚¬Âimage/x-iconÃ¢â‚¬Â>
+<link rel=Ã¢â‚¬Âshortcut iconÃ¢â‚¬Â href=Ã¢â‚¬Âfavicon.icoÃ¢â‚¬Â type=Ã¢â‚¬Âimage/x-iconÃ¢â‚¬Â>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="style.css" rel="stylesheet" media="screen" type="text/css"/>
+<title>Taco Bell Sheffield - America UK &quot;All things American in the UK&quot; </title>
+<style type="text/css">
+<!--
+.style6 {font-size: x-large}
+.style8 {color: #666666}
+-->
+</style>
+</head>
+
+<body>
+<div id="wrapper"> <!-- start of wrapper -->
+<?php include "header.php";?>
+
+<div id="onecolumnleft"><!-- start of div content LEFT -->
+
+
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+  <tr>
+    <td><table width="100%" border="0" cellspacing="10" cellpadding="0">
+      <tr>
+        <th colspan="2" align="left" scope="col"><img src="images/foodanddrink.jpg" width="671" height="221" /></th>
+      </tr>
+      <tr>
+        <th colspan="2" align="left" scope="col"><p align="center" class="style6"> Taco Bell Sheffield </p>
+          <p align="center" class="style6"><a href="http://www.tacobelluk.co.uk" target="_blank"><img src="images/taco-bell-live-mas.jpg" alt="Taco Bell UK" width="340" height="277" border="0" /></a></p>
+          <table width="600" border="0" align="center">
+            <tr>
+              <td><div align="center">
+                <p><strong>UK’S FIRST HIGH STREET TACO BELL ARRIVES ON ‘ECCY’ ROAD</strong><br />
+                  Bringing the Mexican flavour to Sheffield, Taco Bell is here! </p>
+                <p>Taco Bell – the world’s largest Mexican-inspired restaurant chain – will open its doors to the people of South Yorkshire with the launch of its first UK High Street restaurant on the buzzing Ecclesall Road, Sheffield. Bringing with them their famous tacos, burritos and quesadillas, Taco Bell have pulled out all the stops with this state of the art, 80 seat, dual floor restaurant with free wifi and even charging points for customers’ phones. <br />
+                </p>
+                <p>The restaurant design is the first of its kind and marries a youthful, urban look with influences from Mexico and California bringing a spot of sunshine to the streets of South Yorkshire, encouraging locals to ‘Live Más’ or ‘get more out of life’. Customers can look forward to trying something new, with delicious, made-to-order classics like Crunchy Tacos, Crunchwrap Supremes, Grilled Stuft Burritos and Chicken Quesadillas, along with an exclusively adapted menu just for the UK including delicious seasoned Mexican fries and Churros with chocolate sauce.</p>
+                <p>Taco Bell brings with it over fifty years of experience as the market leader in Mexican-style food, and a reputation for quality ingredients, competitive prices, and first rate customer service - which in this restaurant will be provided by 27 eager new employees recruited from the local area. Customers want choice, and Taco Bell brings something different to the High Street, as well as customers’ taste buds, creating an experience and relationship that goes beyond food.</p>
+                <p>Nick Dawson, General Manager, Taco Bell UK &amp; Europe, said:</p>
+                <p>“For years we’ve had fans from cities in the UK requesting a Taco Bell on their High Street, and Sheffield was high up on the hit list of demand.  As the UK’s fifth largest city, with a huge student population, Sheffield is a great location for our first High Street Taco Bell restaurant and we are proud to be partnering with our franchisee Northgate Fast Foods to add a new flavour to the restaurant options in the city.”</p>
+                <p>Part of Yum! Brands, the world’s largest restaurant company, Taco Bell is one of the biggest quick service restaurant brands in the United States serving more than 2 billion customers each year in the US, selling over 2 billion tacos and 1.5 billion burritos in the process. This latest move onto the UK high Street kicks off the start of a wider expansion plan in the UK over the coming years.</p>
+                <p>Currently, there are more than 250 Taco Bell restaurants outside of the United States and approximately 6,500 restaurants in the United States, and Yum! is optimistic about the long–term potential of growing Taco Bell internationally and in the UK as part of that.  To learn more about Taco Bell UK or the Taco Bell menu please go to www.tacobelluk.co.uk or connect with them at Facebook.com/TacoBellUK and @TacoBellUK on Twitter.<br />
+                </p>
+                <p><img src="images/taco-bell-sheffield.jpg" alt="Taco Bell Sheffield" width="595" height="503" /></p>
+              </div>
+                  <div id="tt> </div></div>
+                </div>
+                <p>It is seen by the brand, a sister company to KFC, as a model that could roll out across the rest of the country.</p>
+                <p> Nick Dawson, general manager, Taco Bell UK &amp; Europe, said: “Sheffield is a great place for us to start. It is the UK’s fifth largest city with a thriving student population and Ecclesall Road just felt right. It’s an eating and drinking destination, it’s residential and a nice place. Our franchise partner also has a great local track record.” </p>
+                <p>Taco Bell opens at 10.30am tomorrow and  is targeting the ‘Millennials’, the young and young-at-heart looking for something new and exciting at an affordable price. Tacos, burritos and quesadillas are made to order and cost under £5.</p>
+                <p>&nbsp;</p>
+              </div></td>
+            </tr>
+          </table>
+          <table width="600" border="0" align="center">
+            <tr>
+             
+            </tr>
+            <tr>
+             
+            </tr>
+          </table>
+          <p align="center">&nbsp;</p>
+          <p align="center">&nbsp;</p>
+          <div align="center">
+            <table width="600" border="0">
+              <tr>
+                <td><p align="left"><a href="http://www.therainforestcafe.co.uk" target="_blank"></a><a href="http://www.tacobelluk.co.uk" target="_blank"><img src="images/tacouk.jpg" alt="Taco Bell" width="68" height="79" border="0" /></a></p>
+                  <p align="left"><a href="http://www.facebook.com/hardrock" target="_blank"><img src="images/image002.jpg" alt="Facebook" border="0" /></a><a href="https://www.facebook.com/TacoBellUK" target="_blank" class="style8"> facebook.com/TacoBellUK</a><br />
+                    <a href="http://www.twitter.com/hardrock" target="_blank"><img src="images/image003.jpg" alt="Twitter" border="0" /></a><a href="https://twitter.com/tacobelluk" target="_blank" class="style8"> twitter.com/tacobelluk</a></p></td>
+                <td></td>
+              </tr>
+            </table>
+            </div></th>
+        </tr>
+           <tr>           </tr>
+     
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	    <tr>
+          <td height="104" align="left" valign="top"><p>&nbsp;</p>            </td>
+          <td align="left" valign="top">   <p align="right">&nbsp;</p>
+            <p align="right"><a href="https://members.hardrock.com/rewards-home" target="_blank"></a></p></td>
+	    </tr>
+	  
+	  
+	  
+	  
+	  
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	  <tr>	  </tr>
+      <tr>      </tr>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <tr>      </tr>
+      <tr>
+        <td colspan="2" align="left" valign="top">&nbsp;</td>
+      </tr>
+      <tr>
+        <td colspan="2" align="left" valign="top">&nbsp;</td>
+        </tr>
+    </table></td>
+  </tr>
+</table>
+
+
+
+</div><!-- end of div content LEFT -->
+
+<div id="sidebar"><!-- start of Sidebar -->
+<?php include "sidebar.php";?>
+</div><!-- end of Sidebar -->
+
+
+<div style="clear:both;"></div>
+
+<?php include "footer.php";?>
+
+</body>
+</html>
+
